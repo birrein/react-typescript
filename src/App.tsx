1dfe3e2153da5c84ba9f8e0162e776925953b811
Manuel Marín <birrein@gmail.com>
2022-02-16 01:09:15 -0300

    voy en clase 112

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
-function App() {
+const App: React.FC = () => {
+  const todos = [
+    { id: 't1', text: 'Finish Typescript course' },
+    { id: 't2', text: 'Finish React course' },
+  ];
+
+  const addTodo = (text: string) => {
+    console.log(text);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodo onAddTodo={addTodo} />
+      <TodoList items={todos} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+
+interface NewTodoProps {
+  onAddTodo: (text: string) => void;
+}
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = inputRef.current!.value;
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <label htmlFor="todo-text">Título</label>
+        <input type="text" id="todo-text" ref={inputRef} />
+      </div>
+      <button type="submit">Agregar</button>
+    </form>
+  );
+};
+
+export default NewTodo;
